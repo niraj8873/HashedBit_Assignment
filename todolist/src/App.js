8d4d3e855c1a7fae71daeaf5cfc0359d981a7dff
Niraj Kumar Mishra <nirajkrmishra12378@gmail.com>
2024-05-16 17:36:21 +0530

    todolist

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      if (editIndex !== null) {
+        const updatedTodos = [...todos];
+        updatedTodos[editIndex] = inputValue;
+        setTodos(updatedTodos);
+        setEditIndex(null);
+      } else {
+        setTodos([...todos, inputValue]);
+      }
+      setInputValue('');
+    }
+  };
+
+  const handleEditTodo = (index) => {
+    setInputValue(todos[index]);
+    setEditIndex(index);
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="What Will You Do ?"
+      />
+      <button onClick={handleAddTodo}>
+        {editIndex !== null ? 'Update' : 'Add'}
+      </button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleEditTodo(index)}>Edit</button>
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
